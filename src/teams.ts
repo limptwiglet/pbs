2d7adb9c38c0f0c2308ebde210b0b024928458da
Mark Gerrard <limptwiglet@gmail.com>
2021-08-26 21:45:16 +0100

    extract teams

--- src/teams.ts
+++ src/teams.ts
@@ -1,37 +1,113 @@
-interface Team {
-  averageInitiative: number;
-  players: Player[];
-}
+import { BreakoutModes, PlayerState, Team } from "./common/types";
 
-interface Player {
-  position: {
-    x: number;
-    y: number;
-  };
-  stats: {
-    initiative: number;
-  };
-}
-
-const TEAM1 = (): Team => ({
-  averageInitiative: 0,
+const team1: Team = {
   players: [
     {
-      position: { x: 0, y: 0 },
+      name: "T1 P 1",
+      state: PlayerState.Ready,
+      breakoutMode: BreakoutModes.Moving,
       stats: {
-        initiative: 5,
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T1 P 2",
+      state: PlayerState.Ready,
+      breakoutMode: BreakoutModes.Shooting,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T1 P 3",
+      state: PlayerState.Ready,
+      breakoutMode: BreakoutModes.Shooting,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T1 P 4",
+      state: PlayerState.Ready,
+      breakoutMode: BreakoutModes.Shooting,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T1 P 5",
+      state: PlayerState.Ready,
+      breakoutMode: BreakoutModes.Shooting,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
       },
     },
   ],
-});
+};
 
-const TEAM2 = () => ({
-  averageInitiative: 0,
+const team2: Team = {
   players: [
     {
-      stats: { initiative: 2 },
+      name: "T2 P 1",
+      breakoutMode: BreakoutModes.Moving,
+      state: PlayerState.Ready,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T2 P 2",
+      breakoutMode: BreakoutModes.Moving,
+      state: PlayerState.Ready,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T2 P 3",
+      breakoutMode: BreakoutModes.Shooting,
+      state: PlayerState.Ready,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T2 P 4",
+      breakoutMode: BreakoutModes.Shooting,
+      state: PlayerState.Ready,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
+    },
+    {
+      name: "T2 P 5",
+      breakoutMode: BreakoutModes.Shooting,
+      state: PlayerState.Ready,
+      stats: {
+        speed: 6,
+        initiative: 10,
+        laning: 5,
+      },
     },
   ],
-});
+};
 
-export { TEAM1, TEAM2 };
+export { team1, team2 };
