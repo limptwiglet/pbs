a6486e64c94b36ab5be0b012fcd2e36ed8152d0b
Mark Gerrard <limptwiglet@gmail.com>
2021-08-26 21:38:08 +0100

    Tidy up with prettier

--- src/simulation.ts
+++ src/simulation.ts
@@ -1,21 +1,18 @@
 import { SetupTeam, State, Status, Team } from "./common/types";
 import breakout from "./states/breakout";
-import weightedRandom from "./utils/weighted-random";
 
 const start = (teams: Team[]) => {
   let setupTeams = setup(teams);
 
   let state: State = {
     status: Status.Start,
-    teams: setupTeams
+    teams: setupTeams,
   };
 
   tick(state);
 };
 
 const setup = (teams: Team[]): SetupTeam[] => {
-  // Calculate teams initaive
-
   return teams.map((team) => {
     return {
       initiative:
@@ -30,6 +27,6 @@ const tick = (state: State) => {
   breakout(state);
 };
 
-const end = () => { };
+const end = () => {};
 
 export { start, setup, tick, end };
--- src/states/breakout.ts
+++ src/states/breakout.ts
@@ -1,23 +1,26 @@
-import { State, Player, PlayerState, BreakoutModes, Team } from "../common/types";
+import { State, BreakoutModes, Team } from "../common/types";
 import playerHitsPlayer, { HasHit } from "../utils/player-hits-player";
 
 export default function breakout(state: State) {
-  const shootingPlayers = state.teams.map(team => team.players.filter(p => p.breakoutMode === BreakoutModes.Shooting)).reverse();
-
   teamShootAtOtherTeam(state.teams[0], state.teams[1]);
   teamShootAtOtherTeam(state.teams[1], state.teams[0]);
-};
+}
 
 const teamShootAtOtherTeam = (shootingTeam: Team, targetTeam: Team) => {
-  shootingTeam.players.filter(p => p.breakoutMode === BreakoutModes.Shooting).forEach((player) => {
-    let target = targetTeam.players[Math.floor(Math.random() * targetTeam.players.length)];
+  shootingTeam.players
+    .filter((p) => p.breakoutMode === BreakoutModes.Shooting)
+    .forEach((player) => {
+      let target =
+        targetTeam.players[
+          Math.floor(Math.random() * targetTeam.players.length)
+        ];
 
-    const hasHit = playerHitsPlayer(player, target);
+      const hasHit = playerHitsPlayer(player, target);
 
-    if (hasHit === HasHit.Hit) {
-      console.log(`Player ${player.name} HIT ${target.name}`);
-    } else {
-      console.log(`Player ${player.name} MISSED ${target.name}`);
-    }
-  });
+      if (hasHit === HasHit.Hit) {
+        console.log(`Player ${player.name} HIT ${target.name}`);
+      } else {
+        console.log(`Player ${player.name} MISSED ${target.name}`);
+      }
+    });
 };
